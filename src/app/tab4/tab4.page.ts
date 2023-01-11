4aabdeecf9c7544b252fbbb0a347ab59ba1c8078
Gopi <gopil@live.com>
2023-01-11 12:02:43 +0530

    ..

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -8,19 +8,19 @@ declare let AgoraUIKit: any, document: any;
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
-  appId: string = "633313442061443482e167550875b1e8"
+  appId: string = "fd01ba99948742e2816e54d453dc6e46"
   // role = 'audience' || 'host'
-  role = 'host'
-  token = "00603d680531c6e4d32829fe60bc6405681IAC4Y/gX1hUES6AyRKg7YwKruSarvX4xvlambZAfapu/63ZXrgMAAAAAEAAWylBU0ZcOYgEAAQDOlw5i"
+  role = 'host';
+  token = "007eJxTYDi8ZbHJ4TlR5WWPuddPOBpXzPWPMzWUP3e6uJ6oelfTakcFhrQUA8OkREtLSxMLcxOjVCMLQ7NUU5MUE1PjlGSzVBOzr8Z7kxsCGRkyrvMyMTJAIIjPxlCSWlxSlszAAAAgFx6e";
   // token = '367252ea-ee7e-4dbf-a5a8-b43c35ff8c5e@testMOOD@8640@1642871231'
-  channel = 'testChannel'
+  channel = 'testvc'
 
   constructor() {
    }
 
   ngOnInit() {
     const el = document.querySelector('agora-react-web-uikit');
-    el.addEventListener('agoraUIKitEndcall', (e) => {
+    el.addEventListener(' ', (e) => {
       console.log('hello from html')
       // handle endcall event
       el.remove();
